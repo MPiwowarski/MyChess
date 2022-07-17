6c992292b8da6a92980e0820afc04507e749ee7a
Marcin Piwowarski <marcpiwowarski@hotmail.com>
2022-07-17 14:10:35 +0100

    MakeMove method init

---
+++ MyChess/MyChess.Engine/Constants/HorizontalFieldLabel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyChess.Engine.Constants
+{
+    public enum HorizontalFieldLabel
+    {
+        _1 = 1,
+        _2 = 2,
+        _3 = 3,
+        _4 = 4,
+        _5 = 5,
+        _6 = 6,
+        _7 = 7,
+        _8 = 8,
+    }
+}
---
+++ MyChess/MyChess.Engine/Game.cs
@@ -0,0 +1,48 @@
+﻿using MyChess.Engine.Constants;
+using MyChess.Engine.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyChess.Engine
+{
+    public class Game
+    {
+        public void MakeMove(List<MapFieldDto> gameMap, PlayerColor playerColor,
+            (VerticalFieldLabel x, HorizontalFieldLabel y) from, (VerticalFieldLabel x, HorizontalFieldLabel y) to)
+        {
+            var chessman = gameMap.FirstOrDefault(f => f.X == (int)from.x && f.Y == (int)from.y && f.PlayerColor == playerColor);
+
+            if (chessman == null)
+                throw new Exception(ErrorMessage.InvalidMove);
+
+
+            if (playerColor == PlayerColor.White)
+            {
+                if (chessman.ChessmanType == ChessmanType.Pawn)
+                {
+                    if (to.y < from.y)
+                        throw new Exception(ErrorMessage.PawnBackwardMove);
+
+                }
+            }
+            else
+            {
+
+            }
+
+
+        }
+    }
+
+    public static class ErrorMessage
+    {
+        public const string InvalidMove = "Invalid Move";
+
+        public const string PawnBackwardMove = "Pawn Cannot Move Backward";
+
+    }
+
+}
